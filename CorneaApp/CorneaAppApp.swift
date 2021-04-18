4a8213b8701d60a7f133c0597a6b15e243e54e0d
ykitaguchi <ykitaguchi@gmail.com>
2021-04-18 14:22:25 +0900

    Initial Commit

---
+++ CorneaApp/CorneaAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CorneaAppApp.swift
+//  CorneaApp
+//
+//  Created by Yoshiyuki Kitaguchi on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct CorneaAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
